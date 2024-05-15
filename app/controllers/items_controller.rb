c8b5a3ed08188d25ccdb325e3b1bf96aaacae67e
chihirito <167487411+chihirito@users.noreply.github.com>
2024-05-15 15:23:57 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,11 +6,11 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-  def new 
+  def new
     @item = Item.new
   end
 
-  def create 
+  def create
     Item.create(item_params)
     redirect_to '/'
   end
@@ -21,26 +21,27 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
-  def edit 
+  def edit
   end
 
-  def update 
+  def update
     item = Item.find(params[:id])
     item.update(item_params)
     redirect_to root_path
   end
 
-  def show 
-  end
-  
-  private
-  def item_params
-    params.require(:item).permit(:nickname,:image, :text).merge(user_ide: current_user.id)
+  def show
   end
 
-  def move_to_index 
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  private
+
+  def item_params
+    params.require(:item).permit(:nickname, :image, :text).merge(user_ide: current_user.id)
+  end
+
+  def move_to_index
+    return if user_signed_in?
+
+    redirect_to action: :index
   end
 end
