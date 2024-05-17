c92aec1af753943dadbf7922d8e0c0e6ab28e9df
chihirito <167487411+chihirito@users.noreply.github.com>
2024-05-17 15:34:02 +0900

    アソシエーション、バリデーション

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_one :order
+  belongs_to :category 
+  belongs_to :condition 
+  belongs_to :prefecture 
+  belongs_to :shipping_duration 
+  belongs_to :shipping_fee 
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_duration_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items, only: [:index, :new, :create]
-  root 'items#index'
+  root to: 'items#index'
+  resources :products
 end
