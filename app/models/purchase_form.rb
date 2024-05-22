c80ddcfd5385f0daaf5c02a77af50f7bdac22ff8
chihirito <167487411+chihirito@users.noreply.github.com>
2024-05-22 15:56:34 +0900

    購入フォームの実装

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,14 +1,14 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
+  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number, :token
 
-  with_options presence: ture do
+  with_options presence: true do
     validates :user_id
     validates :product_id
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
-    validates :addresses
+    validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid."}
     validates :token
   end
@@ -21,7 +21,7 @@ class PurchaseForm
       postal_code: postal_code,
       prefecture_id: prefecture_id,
       city: city,
-      addresses: addresses,
+      street_address: street_address,
       building: building,
       phone_number: phone_number,
       purchase_id: purchase.id
